adfb545141d647e08d3061b91c10dc5fb908a932
Ishan Joshi <ijos0001@student.monash.edu>
2017-04-27 16:00:01 +1000

    Board setup, connections graph setup

---
+++ Game.py
@@ -0,0 +1,59 @@
+#Game
+
+#Get the row,col size from user or set automatically
+
+def GetSize():
+    Size = input("Enter [row,column] size seperated by comma.\n")
+    Size = Size.strip()
+    Sizes = Size.split(",")
+    try:
+        row = int(Sizes[0])
+        column = int(Sizes[1])
+        return [row,column]
+    except Exception:
+        print("Invalid input\n")
+        GetSize()
+
+Sizes = GetSize()
+row = Sizes[0]
+column = Sizes[1]
+
+Board = []
+
+connectable = "O"
+filled = "X"
+
+def InitializeBoard():
+    for i in range(0, row):
+        col = [connectable]*column
+        Board.append(col)
+
+def PrintBoard(board):
+    for row in board:
+        values = ""
+        for column in row:
+            values += (str(column) + "\t")
+
+        print(values)
+        print()
+
+InitializeBoard()
+PrintBoard(Board)
+
+Connections = {}
+
+def GetID(i, j):
+    return column*i + j
+
+def GetIndex(n):
+    pass
+
+def InitializeConnections():
+    for i in range(len(Board)):
+        for j in range(len(Board[i])):
+            Connections[GetID(i, j)] = []
+    print(Connections)
+
+
+InitializeConnections()
+
