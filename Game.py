b46771f48c44b5a50d32e58a9d31783aa552e5dd
Ishan Joshi <ijos0001@student.monash.edu>
2017-04-28 10:58:24 +1000

    Gameplay features added

--- Game.py
+++ Game.py
@@ -63,32 +63,69 @@ InitializeConnections()
 
 
 def DrawWithConnections():
+    # print(Connections)
     SymbolsForKey = {}
     for node in Connections:
         if(SymbolsForKey.get(node) is None):
             SymbolsForKey[node] = connectable
         for edges in Connections[node]:
             SymbolsForKey[edges] = filled
-    print(SymbolsForKey)
+    # print(SymbolsForKey)
     output = ""
     for i in range(row*column):
-
         if(i%column==0):
             print(output)
             print()
             output = ""
         output+=(SymbolsForKey[i] + "\t\t")
+    print(output)
 
 
-Connections[1].append(2)
-Connections[2].append(1)
-Connections[2].append(3)
-Connections[3].append(2)
-Connections[0].append(4)
-Connections[4].append(0)
+# Connections[1].append(2)
+# Connections[2].append(1)
+# Connections[2].append(3)
+# Connections[3].append(2)
+# Connections[0].append(4)
+# Connections[4].append(0)
 
-print(Connections)
+# print(Connections)
 
-DrawWithConnections()
+# DrawWithConnections()
 
+players = []
 
+def SetupGame():
+    players = input("Enter player names seperated by comma [Katie, Ishan]\n")
+    players = players.split(",")
+
+    for player in players:
+        print("Welcome, "+player)
+
+    Ended = False
+
+    turn = 0
+    while not Ended:
+        userinput = input("Make a choice, [row:col, row:col] "+players[turn%len(players)]+"\n")
+        if(userinput.lower()=='quit'):
+            print("Game ended\n")
+            Ended = True
+            break
+
+        r1, c1, r2, c2 = 0,0,0,0
+
+        userinput = userinput.split(",")
+        coordinate1 = userinput[0].split(":")
+        r1 = int(coordinate1[0])
+        c1 = int(coordinate1[1])
+        coordinate2 = userinput[1].split(":")
+        r2 = int(coordinate2[0])
+        c2 = int(coordinate2[1])
+
+        Connections[GetID(r1, c1)].append(GetID(r2,c2))
+        Connections[GetID(r2, c2)].append(GetID(r1, c1))
+
+        DrawWithConnections()
+
+        turn += 1
+
+SetupGame()
